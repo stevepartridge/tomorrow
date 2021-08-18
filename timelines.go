57b21c6081dd01a8ca03610c5ad975095bee4d5e
Steve Partridge <srpartridge@gmail.com>
2021-08-17 20:09:25 -0500

    Add GetTemp helper method

---
+++ timelines.go
@@ -0,0 +1,39 @@
+package tomorrow
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// GetTemp is a basic endpoint helper for retreiving temp intervals for a lat/lng combo
+func (c *Client) GetTemp(lat, lng float64, opts ...Option) (Timeline, error) {
+	r := defaultRequest
+	r.Units = c.units
+	r.Lat = lat
+	r.Lng = lng
+
+	for _, opt := range opts {
+		if err := opt(&r); err != nil {
+			return Timeline{}, err
+		}
+	}
+
+	resp, err := c.call(http.MethodGet, "/timelines", nil, map[string]string{
+		"units":     r.Units,
+		"fields":    strings.Join(r.Fields, ","),
+		"timesteps": r.Timesteps,
+		"location":  fmt.Sprintf("%f,%f", r.Lng, r.Lat),
+	})
+
+	if err != nil {
+		return Timeline{}, err
+	}
+
+	if len(resp.Data.Timelines) < 1 {
+		return Timeline{}, errors.New("get.temp: Timelines not found")
+	}
+
+	return resp.Data.Timelines[0], nil
+}
